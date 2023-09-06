9de664d4c0780980a6cb6d49aa5e6702423a3519
Abhishek SAINI <saini.abhishek@hotmail.com>
2023-09-06 20:29:07 +0200

    feat: comment thread

---
+++ src/app/(root)/thread/[id]/page.tsx
@@ -0,0 +1,66 @@
+import { redirect } from 'next/navigation';
+import { currentUser } from '@clerk/nextjs';
+import CardThread from '@/components/thread/card-thread';
+import { GET_USER } from '@/lib/actions/user.actions';
+import { GET_THREAD } from '@/lib/actions/thread.actions';
+import CommentThread from '@/components/thread/comment-thread';
+
+export const revalidate = 0;
+
+type Props = {
+  params: { id: string };
+};
+
+export default async function page({ params }: Props) {
+  if (!params.id) return null;
+
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await GET_USER(user.id);
+  if (!userInfo?.onboarded) redirect('/onboarding');
+
+  const thread = await GET_THREAD(params.id);
+
+  return (
+    <section className="relative">
+      <div>
+        <CardThread
+          id={thread._id}
+          currentUserId={user.id}
+          parentId={thread.parentId}
+          content={thread.text}
+          author={thread.author}
+          community={thread.community}
+          createdAt={thread.createdAt}
+          comments={thread.children}
+        />
+      </div>
+
+      <div className="mt-7">
+        <CommentThread
+          threadId={params.id}
+          currentUserImg={user.imageUrl}
+          currentUserId={JSON.stringify(userInfo._id)}
+        />
+      </div>
+
+      <div className="mt-10">
+        {thread.children.map((childItem: any) => (
+          <CardThread
+            key={childItem._id}
+            id={childItem._id}
+            currentUserId={user.id}
+            parentId={childItem.parentId}
+            content={childItem.text}
+            author={childItem.author}
+            community={childItem.community}
+            createdAt={childItem.createdAt}
+            comments={childItem.children}
+            isComment
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
